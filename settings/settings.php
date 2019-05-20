0b7ded6fedf6e8b8785f28772e7acd128e712dd1
Joseph <50134542+jusephhh@users.noreply.github.com>
2019-05-20 14:22:50 +0800

    Add files via upload

---
+++ settings/settings.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+  <title>Settings</title>
+
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+  <meta name="viewport" content="width=device-width" />
+
+ 
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+
+  <!-- Bootstrap core CSS     -->
+  <link rel="stylesheet" href="https://cdn.rawgit.com/creativetimofficial/now-ui-kit/2e8e665f/assets/css/bootstrap.min.css">
+  <link href="https://cdn.rawgit.com/creativetimofficial/now-ui-kit/2e8e665f/assets/css/now-ui-kit.min.css?v1.2.0" rel="stylesheet"/>
+  <link href="https://cdn.rawgit.com/creativetimofficial/now-ui-kit/2e8e665f/assets/demo/demo.css" rel="stylesheet">
+  <link rel="stylesheet" href="settings.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
+
+  <link rel="icon" href="settings.png" type="image/x-icon" />
+<link rel="shortcut icon" href="settings.png" type="image/x-icon" />
+
+  <style>
+    .navbar .navbar-nav .nav-link:not(.btn) i.fa{
+      font-size: 18px;
+      position: relative;
+      top: 3px;
+      text-align: center;
+      width: 21px;
+    }
+  </style>
+
+</head>
+
+<body class="index-page sidebar-collapse">
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg top">
+        <div class="container">
+            <div class="navbar-translate">
+                <a class="navbar-brand" target="_blank">
+                    Settings
+                </a>
+                <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-bar bar1"></span>
+                    <span class="navbar-toggler-bar bar2"></span>
+                    <span class="navbar-toggler-bar bar3"></span>
+                </button>
+            </div>
+            <div class="collapse navbar-collapse justify-content-end" data-nav-image="./assets/img/blurred-image-1.jpg">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://twitter.com/CreativeTim" target="_blank">Home
+                            <p class="d-lg-none d-xl-none"></p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://twitter.com/CreativeTim" target="_blank">Profile
+                            <p class="d-lg-none d-xl-none"></p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://www.facebook.com/CreativeTim" target="_blank">Settings
+                            <p class="d-lg-none d-xl-none"></p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" target="_blank">Log Out
+                            <p class="d-lg-none d-xl-none"></p>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- End Navbar -->
+
+<!-- content     -->
+<div class="wrapper">
+ <div class="page-header clear-filter">
+  <div class="container">
+   <div class="brand">
+    <h1 id="settings">Settings</h1>
+     <div class="navigation">   
+      <div class="row">
+       <div class="col-xs-12 col-md-3 col-lg-3 left_side">
+        <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+         <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">General</a>
+         <a class="nav-link" id="v-pills-privacy-tab" data-toggle="pill" href="#v-pills-privacy" role="tab" aria-controls="v-pills-privacy" aria-selected="false">Privacy</a>
+        </div>
+       </div>
+       <div class="col-9 right_side">
+        <div class="tab-content" id="v-pills-tabContent">
+         <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
+          
+          <h3 id="header_left">General Account Settings</h3>
+          <hr><div id="to_left">Name <p>Bill Gates</p>
+          <button id="edit_name">edit</button></div><hr>
+          <div id="to_left">Username <p>Billgatesrocks69</p>
+          <button id="edit_username">edit</button></div><hr>
+          <div id="to_left">Password <p>pogisibillgates11</p>
+          <button id="change_password">change password</button></div><hr>
+         </div>
+        
+         <div class="tab-pane fade" id="v-pills-privacy" role="tabpanel" aria-labelledby="v-pills-privacy-tab">
+                <h3 id="header_left">Privacy Settings and Tools</h3>  
+                <hr><div id="to_left">Your Activity <p>Monitor your activity</p>
+                <button>monitor activity</button></div><hr>
+              </div>
+        </div>
+       </div>
+       </div>
+    </div>  
+   </div>
+  </div>
+ </div>
+<!-- content -->
+
+
+<div class="main">
+<footer class="footer">
+      <div class="container">
+       <nav>
+        <ul>
+         <li>
+          <a href="https://github.com/jusephhh">Joseph</a>
+         </li>
+         <li>
+          <a href="https://github.com/zon031997">Zon</a>
+         </li>
+         <li>
+          <a href="https://www.kalskillsakademie.training/front-end-course">Web Dev Class</a>
+         </li>
+         <li>
+          <a href="https://www.kalskillsakademie.training">KSA Certified</a>
+         </li>
+        </ul>
+       </nav>
+        <div class="copyright">&copy;
+                <script>
+                document.write(new Date().getFullYear())
+                </script>, 
+                Made possible by
+                <a id="rightside_footer" href="https://www.kalskillsakademie.training" target="_blank">KSA</a>. Coded by <a id="2rightsidefooter" target="_blank">Joseph & Zon</a>.
+            </div>
+      </div>
+     </footer>
+   </div>
+</body>
+
+<!--   Core JS Files   -->
+<script src="https://cdn.rawgit.com/creativetimofficial/now-ui-kit/2e8e665f/assets/js/core/jquery.min.js"></script>
+<script src="https://cdn.rawgit.com/creativetimofficial/now-ui-kit/2e8e665f/assets/js/core/popper.min.js"></script>
+<script src="https://cdn.rawgit.com/creativetimofficial/now-ui-kit/2e8e665f/assets/js/core/bootstrap.min.js"></script>
+<!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
+<script src="https://cdn.rawgit.com/creativetimofficial/now-ui-kit/2e8e665f/assets/js/plugins/bootstrap-switch.js"></script>
+<!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+<script src="https://cdn.rawgit.com/creativetimofficial/now-ui-kit/2e8e665f/assets/js/plugins/nouislider.min.js"></script>
+<!--  Plugin for the DatePicker, full documentation here: https://github.com/uxsolutions/bootstrap-datepicker -->
+<script src="https://cdn.rawgit.com/creativetimofficial/now-ui-kit/2e8e665f/assets/js/plugins/bootstrap-datepicker.js"></script>
+<!-- Control Center for Now Ui Kit: parallax effects, scripts for the example pages etc -->
+<script src="https://cdn.rawgit.com/creativetimofficial/now-ui-kit/2e8e665f/assets/js/now-ui-kit.js"></script>
+
+</html>
